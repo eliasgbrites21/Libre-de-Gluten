67e1c95f6b92374e054afe451263595f583de2d7
eliasgbrites21 <eliasgbrites@gmail.com>
2023-03-01 16:17:20 -0300

    added button; donde_como & donde_compro

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 
 
 
-document.getElementById("dondeComo").addEventListener("click",()=>redirect());
-document.getElementById("menuLateral").addEventListener("click",()=>goHome());
-
-function redirect(){
-    window.location.href="./donde_como1.html";
-    console.log("funciona")
+let dondeComo=document.getElementById("dondeComo")
+if(dondeComo){
+    dondeComo.addEventListener("click",()=>redirect());
 }
+document.getElementById("menuDiv").addEventListener("click",()=>goHome());
+
 
 function goHome(){
     window.location.href="./index.html";
     console.log("funciona_menu")
 }
+function redirect(){
+    window.location.href="./donde_como1.html";
+    console.log("funciona")
+}
+
+
 
