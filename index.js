76624923febbf02ace87244c476a98ae13cbe052
eliasgbrites21 <eliasgbrites@gmail.com>
2023-03-01 17:08:57 -0300

    all html added

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 
+const dondeComoPag= href="./donde_como1.html";
 
 
 let dondeComo=document.getElementById("dondeComo")
 if(dondeComo){
-    dondeComo.addEventListener("click",()=>redirect());
+    dondeComo.addEventListener("click",()=>redirect(dondeComo));
+}
+
+let dondeCompro=document.getElementById("dondeCompro")
+if(dondeComo){
+    dondeCompro.addEventListener("click",()=>redirect2());
+}
+
+let comoComo=document.getElementById("comoComo")
+if(dondeComo){
+    comoComo.addEventListener("click",()=>redirect3());
+}
+
+let queInvito=document.getElementById("queInvito")
+if(dondeComo){
+    queInvito.addEventListener("click",()=>redirect4());
 }
 document.getElementById("menuDiv").addEventListener("click",()=>goHome());
 
@@ -17,5 +33,16 @@ function redirect(){
     console.log("funciona")
 }
 
+function redirect2(){
+    window.location.href="./donde_compro.html";
+    console.log("funciona")
+}
 
 
+function redirect3(){
+    window.location.href="./como_como.html";
+}
+
+function redirect4(){
+    window.location.href="./que_te_invito.html";
+}
