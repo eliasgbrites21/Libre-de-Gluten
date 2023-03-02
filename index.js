6833d879ae55aa4e509f872a0233e23cd2ca82e2
eliasgbrites21 <eliasgbrites@gmail.com>
2023-03-02 15:57:36 -0300

    js function added

--- index.js
+++ index.js
@@ -1,48 +1,50 @@
 
-const dondeComoPag= href="./donde_como1.html";
+
+const dondeComoPag= "./donde_como1.html";
+const dondeComproPag= href="./donde_compro.html";
+const comoComoPag= href="./como_como.html";
+const queTeInvitoPag= href="./que_te_invito.html";
+const goHome= href="./index.html";
 
 
 let dondeComo=document.getElementById("dondeComo")
 if(dondeComo){
-    dondeComo.addEventListener("click",()=>redirect(dondeComo));
+    dondeComo.addEventListener("click",()=>redirect1(dondeComoPag));
 }
 
 let dondeCompro=document.getElementById("dondeCompro")
 if(dondeComo){
-    dondeCompro.addEventListener("click",()=>redirect2());
+    dondeCompro.addEventListener("click",()=>redirect2(dondeComproPag));
 }
 
 let comoComo=document.getElementById("comoComo")
 if(dondeComo){
-    comoComo.addEventListener("click",()=>redirect3());
+    comoComo.addEventListener("click",()=>redirect3(comoComoPag));
 }
 
 let queInvito=document.getElementById("queInvito")
 if(dondeComo){
-    queInvito.addEventListener("click",()=>redirect4());
+    queInvito.addEventListener("click",()=>redirect4(queTeInvitoPag));
 }
-document.getElementById("menuDiv").addEventListener("click",()=>goHome());
+document.getElementById("menuDiv").addEventListener("click",()=>redirect(goHome));
 
 
-function goHome(){
-    window.location.href="./index.html";
-    console.log("funciona_menu")
-}
 function redirect(){
-    window.location.href="./donde_como1.html";
-    console.log("funciona")
-}
+    window.location.href=goHome;
+     }
 
-function redirect2(){
-    window.location.href="./donde_compro.html";
-    console.log("funciona")
-}
+function redirect1(){
+    window.location.href=dondeComoPag;
+ }
 
+ function redirect2(){
+   window.location.href=dondeComproPag;
+}
 
 function redirect3(){
-    window.location.href="./como_como.html";
+    window.location.href=comoComoPag;
 }
 
 function redirect4(){
-    window.location.href="./que_te_invito.html";
+window.location.href=queTeInvitoPag;
 }
